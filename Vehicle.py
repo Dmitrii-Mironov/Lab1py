fac4838a619bf0444087b40bc1ec29a723e25616
Dmitriy <mirdim777@gmail.com>
2024-11-15 15:00:55 +0300

    class Vehicle

--- Vehicle.py
+++ Vehicle.py
@@ -2,3 +2,10 @@ from typing import List, Optional, Union
 
 class VehicleError(Exception):
     pass
+
+class Vehicle:
+    def __init__(self, make: str, model: str, year: int, color: str) -> None:
+        self.make: str = make #марка транспорта
+        self.model: str = model
+        self.year: int = year
+        self.color: str = color
