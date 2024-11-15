3347471ede188b56fbbcb29e778ca2fa9188a4a5
Dmitriy <mirdim777@gmail.com>
2024-11-15 15:01:44 +0300

    class Car()

--- Vehicle.py
+++ Vehicle.py
@@ -9,3 +9,14 @@ class Vehicle:
         self.model: str = model
         self.year: int = year
         self.color: str = color
+
+class Car(Vehicle):
+    def __init__(self, make: str, model: str, year: int, color: str, doors: int) -> None:
+        super().__init__(make, model, year, color)
+        self.type = "Car"
+        self.doors: int = doors
+    def honk_horn(self) -> str:
+        return "Beep beep!"
+    def get_info(self):
+        return f" {self.type} {self.make} {self.model} {self.year} {self.color} with {self.doors} doors"
+
